719a779da539ed3db9c66e43755814067fc7c95e
lilgvr <71455385+lilgvr@users.noreply.github.com>
2022-05-05 17:56:25 +0400

    cosmetics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import ColumnCtr from "./components/ColumnCtr/ColumnCtr";
 import useTypedSelector from "./hooks/useTypedSelector";
 import React, {Fragment} from 'react';
 import Header from "./components/Header/Header";
+import EmptyCtr from "./components/EmptyCtr/EmptyCtr";
 
 function App() {
     const {categories} = useTypedSelector(state => state.main);
@@ -9,11 +10,15 @@ function App() {
     return (
         <Fragment>
             <Header/>
-            <div className="container">
-                {categories.map((el, i) => {
-                    return <ColumnCtr id={i} key={i}/>
-                })}
-            </div>
+            {categories.length === 0 ?
+                <EmptyCtr/>
+                :
+                <div className="container">
+                    {categories.map((el, i) => {
+                        return <ColumnCtr id={i} key={i}/>
+                    })}
+                </div>
+            }
         </Fragment>
     );
 }
---
+++ src/components/EmptyCtr/EmptyCtr.tsx
@@ -0,0 +1,12 @@
+import {FC} from "react";
+import "./empty-ctr.css";
+
+const EmptyCtr: FC = () => {
+    return (
+        <div className="empty-ctr">
+            <p>Список пуст</p>
+        </div>
+    );
+}
+
+export default EmptyCtr;
