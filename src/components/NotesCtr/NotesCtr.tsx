b18d73f5062dadcbcd254616b30fe81f86d4ad27
lilgvr <71455385+lilgvr@users.noreply.github.com>
2022-05-13 12:31:36 +0400

    memo

--- src/components/NotesCtr/NotesCtr.tsx
+++ src/components/NotesCtr/NotesCtr.tsx
@@ -1,18 +1,21 @@
 import useTypedSelector from "../../hooks/useTypedSelector";
 import Note from "../../containers/Note/Note";
 import {INote} from "../../store/types";
-import React from "react";
+import React, {useCallback, useMemo} from "react";
 import "./notes-ctr.css";
 
 const NotesCtr: React.FC<{ id: number }> = ({id}) => {
+    const {notes} = useTypedSelector(state => state.main);
+
     const sortFunc = (a: INote, b: INote) => {
         const ad = new Date(a.date), bd = new Date(b.date);
         return bd.getTime() - ad.getTime();
     };
-    const filterFunc = (n: INote) => n.categoryId === id;
+    const filterFunc = useCallback((n: INote) => n.categoryId === id, [id]);
 
-    const {notes} = useTypedSelector(state => state.main);
-    const preparedNotes = notes.filter(filterFunc).sort(sortFunc);
+    const memoFunc = useCallback(() => [...notes].filter(filterFunc).sort(sortFunc), [filterFunc, notes]);
+
+    const preparedNotes = useMemo(memoFunc, [memoFunc]);
 
     return (
         <div className="notes-ctr">
